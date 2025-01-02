d37126ab44fda840ded88fc9edd2f6829e5f6fbe
hideffrand <hideffrand@gmail.com>
2025-01-02 16:19:30 +0700

    fix logger file on prod

--- helper/logger.py
+++ helper/logger.py
@@ -8,7 +8,7 @@ def log(origin, msg):
     current_time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
     log_message = f'[{current_time}] {origin} | {msg}\n'
 
-    if os.getenv('DEBUG'):
+    if os.getenv('DEBUG') == 'True':
         os.makedirs(LOG_FOLDER, exist_ok=True)
         log_filename = os.path.join(LOG_FOLDER, f'log_{current_date}.txt')
         with open(log_filename, 'a') as log_file:
