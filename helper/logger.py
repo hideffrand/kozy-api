00e91eba176ff357a91c6aa4026e42fff72a760f
hideffrand <hideffrand@gmail.com>
2025-01-02 07:17:31 +0700

    fixing vercel deployment

--- helper/logger.py
+++ helper/logger.py
@@ -5,15 +5,14 @@ LOG_FOLDER = 'log'
 
 
 def log(origin, msg):
-    # current_date = datetime.now().strftime('%d-%m-%Y')
-
-    # os.makedirs(LOG_FOLDER, exist_ok=True)
-    # log_filename = os.path.join(LOG_FOLDER, f'log_{current_date}.txt')
+    current_date = datetime.now().strftime('%d-%m-%Y')
     current_time = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-
     log_message = f'[{current_time}] {origin} | {msg}\n'
 
-    # with open(log_filename, 'a') as log_file:
-    #     log_file.write(log_message)
+    if os.getenv('DEBUG'):
+        os.makedirs(LOG_FOLDER, exist_ok=True)
+        log_filename = os.path.join(LOG_FOLDER, f'log_{current_date}.txt')
+        with open(log_filename, 'a') as log_file:
+            log_file.write(log_message)
 
     print(log_message)
